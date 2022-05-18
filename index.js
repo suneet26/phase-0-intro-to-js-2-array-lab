1c7446e8e497de46de20c52ee591704946df3635
Sunee Thoedpraiphanawan <amythoedpranawan@yahoo.com>
2022-05-17 22:10:41 -0600

    commit tree 2231d48951390f24af4202156c7c9043c90a93ca

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat() {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop("Garfield")
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift("Milo")
+}
+
+function appendCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.push(name)
+    return copyOfCats
+}
+
+function prependCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.unshift(name)
+    return copyOfCats
+}
+
+function removeLastCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.pop(name)
+    return copyOfCats
+}
+
+function removeFirstCat(name) {
+    const copyOfCats = cats.slice()
+    copyOfCats.shift(name)
+    return copyOfCats
+}
